12406b79002c8ddbc4b893c5f56ac61e350fc483
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-21 17:33:30 +0000

    section-2: change app title

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React from "react";
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-
   return (
-		<div className="App">
-			<div className="container">
-
-				<div className="header-container">
-					<header>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <header>
             <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
+              <p className="title">🧱🚀 Developer DAO Name Service</p>
+              <p className="subtitle">Your D_D record on the blockchain!</p>
             </div>
-					</header>
-				</div>
+          </header>
+        </div>
 
         <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
-}
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
